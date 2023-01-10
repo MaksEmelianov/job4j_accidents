222e052808acc3952d213dfced84051fe06e662d
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2023-01-10 11:10:15 +0500

    0. Spring MVC [#6877]
    Signed-off-by: Maks Emelianov <mr.maksimemelyanov@ya.ru>

--- src/main/java/ru/job4j/accidents/Main.java
+++ src/main/java/ru/job4j/accidents/Main.java
@@ -1,4 +1,9 @@
 package ru.job4j.accidents;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+    public static void main(String[] args) {
+    }
 }
